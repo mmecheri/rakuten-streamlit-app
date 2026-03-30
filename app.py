d27b6e1623964caee7ce3d57e2414d300aa72074
Mourad MECHERI <88212289+mmecheri@users.noreply.github.com>
2026-03-30 12:52:32 +0200

    Change page icon and improve app metadata
    Updated the page icon and enhanced the app description with version and author details.

--- app.py
+++ app.py
@@ -7,19 +7,21 @@ import home,  demo, dataset ,models, conclusion
 # 🧠 Set up the Streamlit app config BEFORE anything else
 st.set_page_config(
     page_title="Data Science Project: Product Categorization",
-    page_icon="🔀",
+    page_icon="🏷️",
     # page_icon='./doc/icon_app.png',
     
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={    
+layout="wide",
+initial_sidebar_state="expanded",
+menu_items={    
     'Report a bug': 'https://github.com/mmecheri/Data_Scientist_Rakuten_Project/issues',
     'About': '''
-    # Rakuten Product Classification  
-    A multimodal classification project using product text and image data.  
-    Version: 1.0.0  
-    Author: Mourad MECHERI  
-    '''
+# Rakuten Product Classification  
+A multimodal classification project using product text and image data.  
+
+Version: 1.0.0  
+Python: 3.10  
+Author: M. Mecheri  
+'''
 }
 )
 
